a69774a0b44f9592f2728c5a30d039a68c16f193
esperanceahouissou <esperance.ahouissou@epitech.eu>
2026-02-05 12:56:51 +0100

    v1

--- docker/start.sh
+++ docker/start.sh
@@ -14,10 +14,6 @@ php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
-# Configurer le port Apache
-sed -i 's/Listen 80/Listen 8080/g' /etc/apache2/ports.conf
-sed -i 's/:80/:8080/g' /etc/apache2/sites-available/000-default.conf
-
 # Démarrer Apache
 echo "Starting Apache..."
-apache2-foreground
+exec apache2-foreground
